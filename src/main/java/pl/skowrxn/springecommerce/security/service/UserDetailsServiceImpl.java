6b12b488bdd1a6bf1b102eb9a2cd184a450ac829
Bartłomiej <bartomiejskowron@icloud.com>
2025-04-19 12:58:25 +0200

    Implement UserDetailsServiceImpl

---
+++ src/main/java/pl/skowrxn/springecommerce/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package pl.skowrxn.springecommerce.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.skowrxn.springecommerce.entity.User;
+import pl.skowrxn.springecommerce.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username = " + username)
+        );
+        return UserDetailsImpl.build(user);
+    }
+}
