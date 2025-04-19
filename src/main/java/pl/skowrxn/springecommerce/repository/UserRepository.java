e4d6367a6bdb3a8993b1a362ef790a5e9c1c711f
Bartłomiej <bartomiejskowron@icloud.com>
2025-04-19 14:34:39 +0200

    Add methods to check existence of username and email in UserRepository

--- src/main/java/pl/skowrxn/springecommerce/repository/UserRepository.java
+++ src/main/java/pl/skowrxn/springecommerce/repository/UserRepository.java
@@ -2,6 +2,7 @@ package pl.skowrxn.springecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.skowrxn.springecommerce.entity.Role;
 import pl.skowrxn.springecommerce.entity.User;
 
 import java.util.Optional;
@@ -12,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUsername(String username);
+
+    Boolean existsByUsernameIgnoreCase(String email);
+
+    Boolean existsByEmailIgnoreCase(String username);
+
 }
