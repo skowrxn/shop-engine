f13f0cdc294bef3eaee219ccc96a673abb5c355f
Bartłomiej <bartomiejskowron@icloud.com>
2025-04-23 14:17:06 +0200

    Add Cart entity

---
+++ src/main/java/pl/skowrxn/springecommerce/entity/Cart.java
@@ -0,0 +1,35 @@
+package pl.skowrxn.springecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="carts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CartItem> items = new ArrayList<>();
+
+    private Double totalPrice = 0.0;
+
+
+
+}
