f4e8522268722de0aebaef7ee89a0705f318876f
Bartłomiej <bartomiejskowron@icloud.com>
2025-04-23 14:41:42 +0200

    Add CartItemDTO class

---
+++ src/main/java/pl/skowrxn/springecommerce/dto/CartItemDTO.java
@@ -0,0 +1,22 @@
+package pl.skowrxn.springecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CartItemDTO {
+
+    private Long id;
+    private Long cartId;
+    private ProductDTO product;
+    private Double singlePrice;
+    private Double totalPrice;
+    private Double discount;
+    private Integer quantity;
+
+}
