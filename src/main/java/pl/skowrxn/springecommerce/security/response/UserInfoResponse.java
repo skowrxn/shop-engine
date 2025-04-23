04c503809e2ce1298a7b488b14eb69cd30a0742e
Bartłomiej <bartomiejskowron@icloud.com>
2025-04-23 14:15:09 +0200

    Add email field and constructor to UserInfoResponse

--- src/main/java/pl/skowrxn/springecommerce/security/response/UserInfoResponse.java
+++ src/main/java/pl/skowrxn/springecommerce/security/response/UserInfoResponse.java
@@ -13,6 +13,13 @@ public class UserInfoResponse {
 
     private Long id;
     private String username;
+    private String email;
     private List<String> roles;
+
+    public UserInfoResponse(Long id, String username, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
 
 }
