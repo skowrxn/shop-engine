42dc5c13ec503b8dbff82395979508272ae5d328
Bartłomiej <bartomiejskowron@icloud.com>
2025-04-23 20:42:26 +0200

    Add Address entity with fields

--- src/main/java/pl/skowrxn/springecommerce/entity/Address.java
+++ src/main/java/pl/skowrxn/springecommerce/entity/Address.java
@@ -1,11 +1,15 @@
 package pl.skowrxn.springecommerce.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "addresses")
+@Getter
+@Setter
 public class Address {
 
     @Id
@@ -15,5 +19,13 @@ public class Address {
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    private String street;
+    private String city;
+    private String province;
+    private String country;
+    private String postalCode;
+    private String phoneNumber;
+    private boolean defaultAddress;
 
 }
