4ff1e0b207fc467c8aef0f8f751b47d8c16d771b
Bartłomiej <bartomiejskowron@icloud.com>
2025-04-23 18:41:51 +0200

    Implement CartController

---
+++ src/main/java/pl/skowrxn/springecommerce/controller/CartController.java
@@ -0,0 +1,48 @@
+package pl.skowrxn.springecommerce.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.skowrxn.springecommerce.dto.CartContentResponse;
+import pl.skowrxn.springecommerce.dto.CartItemDTO;
+import pl.skowrxn.springecommerce.entity.Cart;
+import pl.skowrxn.springecommerce.service.CartService;
+
+@RestController
+public class CartController {
+
+     private final CartService cartService;
+
+     public CartController(CartService cartService) {
+         this.cartService = cartService;
+     }
+
+     @PostMapping("/cart/add")
+     public ResponseEntity<?> addToCart(@RequestParam Long productId, @RequestParam Integer quantity) {
+         CartItemDTO cartItemDTO = cartService.addToCart(productId, quantity);
+         return ResponseEntity.ok(cartItemDTO);
+     }
+
+     @DeleteMapping("/cart/remove/{cartItemId}")
+     public ResponseEntity<?> removeFromCart(@PathVariable Long cartItemId) {
+         cartService.removeFromCart(cartItemId);
+         return ResponseEntity.noContent().build();
+     }
+
+     @PutMapping("/cart/update/{cartItemId}")
+     public ResponseEntity<CartItemDTO> updateCartItemQuantity(@PathVariable Long cartItemId, @RequestParam Integer quantity) {
+         CartItemDTO cartItemDTO = cartService.updateCartItemQuantity(cartItemId, quantity);
+         return ResponseEntity.ok(cartItemDTO);
+     }
+
+     @GetMapping("/cart/content")
+     public ResponseEntity<CartContentResponse> getCartContent() {
+         return ResponseEntity.ok(cartService.getCartContent());
+     }
+
+     @PostMapping("/cart/clear")
+     public ResponseEntity<?> clearCart() {
+         this.cartService.clearCart();
+         return ResponseEntity.noContent().build();
+     }
+
+}
