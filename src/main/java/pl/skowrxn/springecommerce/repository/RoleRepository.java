55d9ffef55e7e6889262e9d86c7f3afbb1e924cd
Bartłomiej <bartomiejskowron@icloud.com>
2025-04-28 17:48:37 +0200

    Change return type to List for multiple role retrieval

--- src/main/java/pl/skowrxn/springecommerce/repository/RoleRepository.java
+++ src/main/java/pl/skowrxn/springecommerce/repository/RoleRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.skowrxn.springecommerce.entity.Role;
 import pl.skowrxn.springecommerce.entity.RoleType;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Optional<Role> findRoleByRoleType(RoleType roleType);
+    List<Role> findRolesByRoleType(RoleType roleType);
 
 }
+
