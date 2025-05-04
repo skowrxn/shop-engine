5bae502e1716b4ea57716db31595f65bf9af6b42
Bartłomiej <bartomiejskowron@icloud.com>
2025-05-04 23:42:30 +0200

    Add new removeFromCart method to avoid unnecessary repository calls

--- src/main/java/pl/skowrxn/springecommerce/service/CartService.java
+++ src/main/java/pl/skowrxn/springecommerce/service/CartService.java
@@ -2,12 +2,15 @@ package pl.skowrxn.springecommerce.service;
 
 import pl.skowrxn.springecommerce.dto.response.CartContentResponse;
 import pl.skowrxn.springecommerce.dto.CartItemDTO;
+import pl.skowrxn.springecommerce.entity.CartItem;
 
 public interface CartService {
 
     CartItemDTO addToCart(Long productId, Integer quantity);
 
     void removeFromCart(Long cartItemId);
+
+    void removeFromCart(CartItem cartItem);
 
     void clearCart();
 
